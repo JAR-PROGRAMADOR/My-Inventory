ae033a3f92d3242e425e16cd70e143b92995d492
unknown <jerplay6@gmail.com>
2026-09-15 00:19:20 -0500

    fix frontend categorias2

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import mysql from 'mysql2';
-import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-app.use(cors());
-app.use(express.static(__dirname));
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname)));
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
